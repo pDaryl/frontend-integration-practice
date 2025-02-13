ce04be337d8e1756e838a8aaf2ca9d3eff66fe87
Daryl Peterson <pdaryl8@gmail.com>
2025-02-13 14:22:48 -0600

    created project to show and hide text

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function randomColor(){
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
+const text = document.getElementById("text");
+const button = document.getElementById("btn");
 
-    return `rgb(${r}, ${g}, ${b})`;
-}
-
-function changeColor(){
-    document.body.style.backgroundColor = randomColor();
-    document.getElementById("theColor").innerText = randomColor()
+function manipulateText(){
+    if(text.style.display === "none"){
+        text.style.display = "block";
+        button.innerText = "Hide text"; 
+    } else {
+        text.style.display = "none";
+        button.innerText = "Show text";
+    }
 }
