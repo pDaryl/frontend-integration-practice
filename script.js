eb50e830fe61cd731536144d9da399fdee0aaf98
Daryl Peterson <pdaryl8@gmail.com>
2025-02-13 17:20:19 -0600

    created project to count word and characters

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const counter = document.getElementById("counter");
-const characters = document.getElementById("charField");
+const inputField = document.getElementById("inputField");
+const wordCountDisplay = document.getElementById("word-Counter");
+const characters = document.getElementById("char-Counter");
 
-characters.addEventListener("input", function(){
-    counter.textContent = `Character Count: ${characters.value.length}`;
+inputField.addEventListener("input", function(){
+    const words = inputField.value.trim().split(/\s+/);
+    const wordCount = inputField.value.trim() === "" ? 0 : words.length;
+
+    wordCountDisplay.textContent = `Word Count: ${wordCount}`;
+     characters.textContent = `Character Count: ${inputField.value.length}`
+
+    if(words.length > 50){
+        alert("you have exceeded the max amount of words. (50 words)");
+    }
 });
